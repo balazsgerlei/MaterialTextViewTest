4e1db33b22f990b0d3eff43ec5427254787325b3
Balázs Gerlei <gerlei.balazs@autsoft.hu>
2016-11-11 16:09:06 +0100

    - Added test project to illustrate the bug of the Library: If you don’t set the detail in Storyboard, it won’t appear when later setting it in code (when clicking to the button in this case)

---
+++ MaterialTest/MaterialTest/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  MaterialTest
+//
+//  Created by Balazs Gerlei on 2016. 11. 11..
+//  Copyright © 2016. Balazs Gerlei. All rights reserved.
+//
+
+import UIKit
+import Material
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textFieldSomething: TextField!
+    @IBOutlet weak var textFieldSomethingElse: TextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onClickyButtonTouchUpInside(_ sender: Any) {
+        textFieldSomething.detail = "Error!"
+        textFieldSomething.detailColor = UIColor.red
+        
+        textFieldSomething.detail = "Error!"
+        textFieldSomething.detailColor = UIColor.red
+    }
+
+}
+
